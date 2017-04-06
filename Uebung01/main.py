0b6c60df88b4a86291fedb3713cce1dc189f4666
fepeter <fepeter@htwg-konstanz.de>
2017-04-06 13:28:21 +0200

    Bilinear Interpolation works

--- Uebung01/main.py
+++ Uebung01/main.py
@@ -26,17 +26,13 @@ def calcM(Winkel=0, OffsetX=0, OffsetY=0, ZerrungX=0, ZerrungY=0, StreckungX=1, 
     return retM
 
 
-m = calcM(0, 0, -0, -0, 0, 0.5, 1)
-
-
 def coordTransform(x, y, m):
     newX = m[0, 0] * x + m[0, 1] * y - m[0, 2]
     newY = m[1, 0] * x + m[1, 1] * y + m[1, 2]
     return (newX, newY)
 
 
-def getPx(x, y, img, m):
-    (srcX, srcY) = coordTransform(x, y, m)
+def getPx(srcX, srcY, img):
     if srcX < 0 or srcY < 0:
         return 0
     if srcX >= img.shape[1] or srcY >= img.shape[0]:
@@ -44,32 +40,44 @@ def getPx(x, y, img, m):
     return img[srcY, srcX]
 
 
-def applyTransform(img, M, BilinearInterp):
+def applyTransform(img, m, BilinearInterp):
     target = np.zeros(img.shape)
     print(target.shape)
     for y in range(0, img.shape[0]):
         for x in range(0, img.shape[1]):
-            target[y, x] = getPx(x, y, img, m)
+            (srcX, srcY) = coordTransform(x, y, m)
+            if BilinearInterp:
+                midX = int(srcX + 1)
+                midY = int(srcY + 1)
+                target[y, x] = getPx(srcX, srcY, img) * (midX - srcX) * (midY - srcY) + \
+                               getPx(srcX + 1, srcY, img) * (srcX + 1 - midX) * (midY - srcY) + \
+                               getPx(srcX, srcY + 1, img) * (midX - srcX) * (srcY + 1 - midY) + \
+                               getPx(srcX + 1, srcY + 1, img) * (srcX + 1 - midX) * (srcY + 1 - midY)
+            else:
+                target[y, x] = getPx(srcX, srcY, img)
     return target
 
 
 def main():
+    m = calcM(5, 0, -0, -0, 0, 1, 1)
     img = scipy.misc.imread(name="gletscher.jpg", flatten=True)
     RGBimg = scipy.misc.imread(name="ambassadors.jpg", )
     print(img.shape)
     print(RGBimg.shape)
 
-    #Transform gletscher.jpg
+    # Transform gletscher.jpg
     out1 = applyTransform(img, m, True)
 
-    #Transform ambassadors.jpg
+    # Transform ambassadors.jpg
     RGBnew = RGBimg
     for i in range(3):
         print("Run Color" + str(i))
-        RGBnew[:,:,i] = applyTransform(RGBimg[:,:,i], m, True)
+        RGBnew[:, :, i] = applyTransform(RGBimg[:, :, i], m, False)
 
+    plt.ion()
     plt.imshow(RGBnew)
-    plt.show()
+    plt.show(block=True)
+
 
 if __name__ == "__main__":
     main()
