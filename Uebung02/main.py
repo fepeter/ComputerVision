a6fe963942361490d17b5838789625968fe47a2f
fepeter <fepeter@htwg-konstanz.de>
2017-04-21 14:35:24 +0200

    AffinTrans Class

--- Uebung02/main.py
+++ Uebung02/main.py
@@ -2,14 +2,16 @@ import numpy as np
 import scipy.misc
 import scipy
 import matplotlib.pyplot as plt
+from affinTransformation import AffinTransformation
 import math
-
 
 def main():
     #Übung 2
     print("Übung 2")
     img = scipy.misc.imread(name="schraegbild_tempelhof.jpg", flatten=True)
 
+    AT = AffinTransformation()
+    AT.transform()
     plt.gray()
     plt.imshow(img)
     plt.show()
